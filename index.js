943299ad90faecac8864cde8c50ac0d8b9211659
JoseArjona <j.angel.matheis@gmail.com>
2024-02-11 17:46:40 -0600

    ✅ extraName en folders personalizado

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 /* import path from 'node:path' */
-import readline from 'node:readline'
+import readline from 'node:readline/promises'
 import { organize } from './utils/orderByType.js'
 
 console.log('Bienvenido a ORDENODE')
-console.log('Pasa la ruta de la carpeta que deseas ordenar. Importante el programa no funciona en carpetas. \nPor motivos técnicos las carpetas creadas tendrán el nombre de Folder-byOrdeNode, puedes retirar el nombre a posterior')
+console.log('Pasa la ruta de la carpeta que deseas ordenar. Importante el programa no funciona en carpetas. \nPor motivos técnicos las carpetas creadas tendrán el nombre de Folder-nombreextra, puedes retirar el nombre a posterior')
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-reader.question('Introduce la ruta de la carpeta que deseas ordenar: ', (ruta) => {
-  console.log(`Ruta seleccionada: ${ruta}`)
+const questions = async () => {
+  const path = await reader.question('Introduce la ruta de la carpeta que deseas ordenar: ')
+  const extraName = await reader.question('Introduce la terminación de la carpeta: ')
+  console.log(`Ruta seleccionada: ${path} \nTerminación de la carpeta: ${extraName} \nVista previa de carpeta: Documentos-${extraName}`)
+  const confirm = await reader.question('¿Deseas continuar con el ordenamiento? (S/N): ')
+  if (confirm.toLowerCase() === 's') {
+    organize(path, extraName)
+    reader.close()
+  } else {
+    console.log('Ordenamiento cancelado.')
+    reader.close()
+  }
+}
 
+questions()
+
+/* reader.question('Introduce la ruta de la carpeta que deseas ordenar: ', async (ruta) => {
+  const extraName = await reader.question('Introduce la terminación de la carpeta')
+  console.log(`Ruta seleccionada: ${ruta} \nTerminación de la carpeta: ${extraName} \nVista previa de carpeta: Documentos-${extraName}`)
   reader.question('¿Deseas continuar con el ordenamiento? (S/N): ', (confirm) => {
     if (confirm.toLowerCase() === 's') {
       organize(ruta)
@@ -23,7 +39,7 @@ reader.question('Introduce la ruta de la carpeta que deseas ordenar: ', (ruta) =
     }
   })
 })
-
+ */
 /* const __dirname = path.resolve()
 console.log(__dirname)
 
--- utils/orderByType.js
+++ utils/orderByType.js
@@ -5,17 +5,18 @@ import fs from 'node:fs'
 
 const fileTypes = JSON.parse(fs.readFileSync('./utils/fileTypes.json'))
 
-/*
+/**
  * @name organize
  * @constructor
  * @param {string} dirname - La ruta del directorio.
+ * @param {string} extraName - El nombre adicional de la carpeta.
 */
-export const organize = async (dirname) => {
+export const organize = async (dirname, extraName) => {
   fileTypes.forEach(async (type) => {
     const files = await readFiles(dirname, type.extensions)
     try {
       if (files.length === 0) return console.log('')
-      const folderName = type.name + '-Ordenode'
+      const folderName = `${type.name}-${extraName}`
       const folderPath = path.join(dirname, folderName)
       const response = await createFolder(folderPath)
       if (response === 'Ya existe') return console.log('Carpeta ya existente')
